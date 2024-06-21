fd0465fcab3f78ba383ab6d0d3bb43f2fc69668f
Simon <saymon_set@hotmail.com>
2024-06-21 08:57:53 -0400

    Instalo y run first

--- README.txt
+++ README.txt
@@ -1,7 +1,31 @@
+# Create virtual enviroment
+python3 -m venv venv
+
+#Activa virtual enviroment
+source venv/bin/activate
+
+#Instalamos los requerimientos
+ pip3 install -r requirements.txt 
+
+#Arrancamos la aplicacion
+flask --app flaskr run --debug --port 8000
+
+
+#--------------otros----------
+
+#Caso de bug
+1-)  pip3 freeze > requirements.txt
+2-)   pip3 uninstall -r requirements.txt 
+3-) source venv/bin/activate
+4-) pip3 install -r requirements.txt 
+5-) flask --app flaskr run --debug --port 8000
+
+
+
 export FLASK_APP=flaskr
 
  pip3 freeze > requirements.txt
- pip3 install -r requirements.txt 
+
 
 # Forma para comenzar sencillo
 flask --app app run --port 8000
--- app.py
+++ app.py
@@ -1,26 +1,22 @@
-from flask import Flask
-from dotenv import load_dotenv
-from flask_cors import CORS
-import os
-from config.mongodb import mongo
-from routes import blueprint
-from flask_jwt_extended import JWTManager
-# import strawberry
-# from ariadne import load_schema_from_path, make_executable_schema, \
-#     graphql_sync, snake_case_fallback_resolvers, ObjectType
-# from ariadne.explorer import ExplorerGraphiQL
-
-
-load_dotenv()
-app = Flask(__name__)
-app.config['MONGO_URI'] = os.getenv('MONGO_URI')
-app.config['RESTPLUS_MASK_SWAGGER'] = False
-app.config["JWT_SECRET_KEY"] = os.getenv('JWT_SECRET')
-CORS(app, resources={r"*": {"origins": "*"}})  # Ajusta según tus rutas y necesidades
-# app.config["JWT_ACCESS_TOKEN_EXPIRES"] = datetime.timedelta(hours=os.getenv('JWT_EXPIRE_TIME'))
+# from flask import Flask
+# from dotenv import load_dotenv
+# from flask_cors import CORS
+# import os
+# from config.mongodb import mongo
+# from routes import blueprint
+# from flask_jwt_extended import JWTManager
  
-jwt = JWTManager(app)
-mongo.init_app(app)
-app.register_blueprint(blueprint)
+
+
+# load_dotenv()
+# app = Flask(__name__)
+# app.config['MONGO_URI'] = os.getenv('MONGO_URI')
+# app.config['RESTPLUS_MASK_SWAGGER'] = False
+# app.config["JWT_SECRET_KEY"] = os.getenv('JWT_SECRET')
+# CORS(app, resources={r"*": {"origins": "*"}})  # Ajusta según tus rutas y necesidades
+ 
+# jwt = JWTManager(app)
+# mongo.init_app(app)
+# app.register_blueprint(blueprint)
 
  
